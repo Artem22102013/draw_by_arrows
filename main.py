443fe5e297cde60fb9c42371f9071a1de2c55080
Artem <a.e.makarov13@gmail.com>
2023-01-23 21:43:54 +0800

    I done my game! I will test it now

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import turtle
-import arrows
 t = turtle.Pen()
-print("ok")
+def f():
+    t.forward(50)
+def l():
+    t.left(90)
+def r():
+    t.right(90)
+while True:
+    t.onkey(f, "Up")
+    t.onkey(l, "Left")
+    t.onkey(r, "Right")
