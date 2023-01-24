bf9bd9b9c84421cb44630f2f16ba481993da0b34
Artem <a.e.makarov13@gmail.com>
2023-01-24 20:53:33 +0800

    i done my multiply test! everyone test it!

---
+++ Math/multiply_test.py
@@ -0,0 +1,8 @@
+import random
+r = random
+print("Тренировка Умножения")
+while yours_c != 1!:
+    a = r.randint(2, 9)
+    b = r.randint(2, 9)
+    c = a * b
+    yours_c = eval(input(f"Сколько будет {a} * {b}? "))
