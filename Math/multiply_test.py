e0fb05b0b953598467fd9f187cb30279abb1b347
Artem <a.e.makarov13@gmail.com>
2023-01-24 20:55:00 +0800

    i done!

--- Math/multiply_test.py
+++ Math/multiply_test.py
@@ -6,3 +6,7 @@ while yours_c != 1!:
     b = r.randint(2, 9)
     c = a * b
     yours_c = eval(input(f"Сколько будет {a} * {b}? "))
+    if yours_c == c:
+        print("Правильно!")
+    else:
+        print("Неправильно!")
